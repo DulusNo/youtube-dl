b2a83052825e5f5780c39b28d18883acf7ce5910
Dulus_No <35026145+DulusNo@users.noreply.github.com>
2018-07-07 08:05:52 +0900

    Update trutv.py

--- youtube_dl/extractor/trutv.py
+++ youtube_dl/extractor/trutv.py
@@ -2,11 +2,13 @@
 from __future__ import unicode_literals
 
 import re
+import codecs
 
 from .turner import TurnerBaseIE
 
 from ..utils import (
     str_or_none,
+    int_or_none,
 )
 
 class TruTVIE(TurnerBaseIE):
@@ -26,36 +28,55 @@ class TruTVIE(TurnerBaseIE):
     def _real_extract(self, url):
         path, video_id = re.match(self._VALID_URL, url).groups()
         auth_required = False
+        webpage = self._download_webpage(url, video_id or path)
+        info = {}
+        initial_data = {}
         if path:
             data_src = 'http://www.trutv.com/video/cvp/v2/xml/content.xml?id=%s.xml' % path
+            info = self._extract_cvp_info(
+                data_src, path, {
+                    'default': {
+                        'media_src': 'http:',
+                    },
+                })
+            media_id = self._search_regex(
+                r"TTV\.video\.clip\.mediaId\s*=\s*'([^']+)';",
+                webpage, 'media id', default=None)
         else:
-            webpage = self._download_webpage(url, video_id)
-            video_id = self._search_regex(
+            raw_data = self._html_search_regex(
+                r"TTV\.TVE\.analytics\s*=\s*'([^']+)';",
+                webpage, 'initial data')
+            initial_data = self._parse_json(codecs.decode(raw_data.encode(), 'unicode_escape'), video_id)
+            video_id = initial_data.get('id') or self._search_regex(
                 r"TTV\.TVE\.episodeId\s*=\s*'([^']+)';",
                 webpage, 'video id', default=video_id)
             auth_required = self._search_regex(
                 r'TTV\.TVE\.authRequired\s*=\s*(true|false);',
                 webpage, 'auth required', default='false') == 'true'
-            media_id = self._search_regex(
+            media_id = initial_data.get('mediaId') or self._search_regex(
                 r"TTV\.TVE\.mediaId\s*=\s*'([^']+)';",
                 webpage, 'media id', default=None)
-            title = self._search_regex(
+            title = initial_data.get('title') or self._search_regex(
                 r"<div\s+class\s*=\s*\"clip-title\"[^>]*>([^<]*)</div>",
                 webpage, 'title', default=video_id)
-            description = self._search_regex(
-                r"<div\s+class\s*=\s*\"clip-description\"[^>]*>([^<]*)</div>",
-                webpage, 'title', default=None)
 
-        info = self._extract_ngtv_info(
+        formats = info.pop('formats', [])
+        info.update(self._extract_ngtv_info(
             media_id, None, {
                 'url': url,
                 'site_name': 'truTV',
                 'auth_required': auth_required,
-            })
+            }))
+        formats.extend(info.pop('formats', []))
+        self._sort_formats(formats)
 
         info.update({
-            'title': str_or_none(title),
-            'description': str_or_none(description),
+            'formats': formats,
+            'title': info.get('title') or str_or_none(title),
+            'description': info.get('description') or str_or_none(initial_data.get('description')),
+            'series': info.get('series') or str_or_none(initial_data.get('franchise')),
+            'season_number': info.get('season_number') or int_or_none(initial_data.get('seasonNumber')),
+            'episode_number': info.get('episode_number') or int_or_none(initial_data.get('episodeNumber')),
         })
 
         return info
