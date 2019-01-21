8c889e8a0f70a4cb29d6ce656081affec066559f
Dulus_No <35026145+DulusNo@users.noreply.github.com>
2019-01-21 12:55:18 +0900

    Update adultswim.py

--- youtube_dl/extractor/adultswim.py
+++ youtube_dl/extractor/adultswim.py
@@ -1,19 +1,24 @@
 # coding: utf-8
 from __future__ import unicode_literals
 
+import base64
 import re
+import time
 
 from .turner import TurnerBaseIE
 from ..utils import (
     int_or_none,
     float_or_none,
+    parse_iso8601,
     strip_or_none,
     str_or_none,
+    ExtractorError,
+    unified_timestamp,
 )
 
 
 class AdultSwimIE(TurnerBaseIE):
-    _VALID_URL = r'https?://(?:www\.)?adultswim\.com/videos/(?P<show_path>[^/?#]+)(?:/(?P<episode_path>[^/?#]+))?'
+    _VALID_URL = r'https?://(?:www\.)?adultswim\.com/(?P<as_type>videos|streams)/(?P<show_path>[^/?#]+)?(?:/(?P<episode_path>[^/?#]+))?'
 
     _TESTS = [{
         'url': 'http://adultswim.com/videos/rick-and-morty/pilot',
@@ -82,20 +87,21 @@ class AdultSwimIE(TurnerBaseIE):
     }]
 
     def _real_extract(self, url):
-        show_path, episode_path = re.match(self._VALID_URL, url).groups()
-        display_id = episode_path or show_path
+        as_type, show_path, episode_path = re.match(self._VALID_URL, url).groups()
+        display_id = episode_path or show_path or as_type
         webpage = self._download_webpage(url, display_id)
         initial_data = self._parse_json(self._search_regex(
-            r'AS_INITIAL_DATA(?:__)?\s*=\s*({.+?});',
+            #r'AS_INITIAL_DATA(?:__)?\s*=\s*({.+?});',
+            r'(?:NEXT_DATA|INITIAL_STATE)(?:__)?\s*=\s*({.+?});',
             webpage, 'initial data'), display_id)
 
         info = {}
-        is_stream = show_path == 'streams'
+        is_stream = as_type == 'streams'
         if is_stream:
-            if not episode_path:
-                episode_path = 'live-stream'
+            if not show_path:
+                show_path = 'live-stream'
 
-            video_data = next(stream for stream_path, stream in initial_data['streams'].items() if stream_path == episode_path)
+            video_data = next(stream for stream in initial_data['streams'] if stream['id'] == show_path)
             video_id = video_data.get('stream')
 
             if not video_id:
@@ -110,39 +116,61 @@ class AdultSwimIE(TurnerBaseIE):
                     entries, video_data.get('id'), video_data.get('title'),
                     strip_or_none(video_data.get('description')))
         else:
-            show_data = initial_data['show']
+            props = initial_data['props']
+            apollo_state = props['__APOLLO_STATE__']
 
             if not episode_path:
                 entries = []
-                for video in show_data.get('videos', []):
-                    slug = video.get('slug')
-                    if not slug:
-                        continue
-                    entries.append(self.url_result(
-                        'http://adultswim.com/videos/%s/%s' % (show_path, slug),
-                        'AdultSwim', video.get('id')))
+                playlist_title = None
+                playlist_id = None
+                playlist_description = None
+                for k, v in apollo_state.items():
+                    slug = v.get('slug')
+                    if k.startswith('Video:') and slug:
+                        entries.append(self.url_result(
+                            'http://adultswim.com/videos/%s/%s' % (show_path, slug),
+                            'AdultSwim', v.get('_id')))
+                    if not playlist_id and k.startswith('VideoCollection:'):
+                        playlist_description = v.get('description')
+                        playlist_id = base64.b64decode(v.get('id', '')).decode('ascii').split(':')[-1]
+                        playlist_title = v.get('title')
+                    
                 return self.playlist_result(
-                    entries, show_data.get('id'), show_data.get('title'),
-                    strip_or_none(show_data.get('metadata', {}).get('description')))
+                    entries, playlist_id, playlist_title,
+                    strip_or_none(playlist_description))
 
-            video_data = show_data['sluggedVideo']
-            if len(video_data['media_id']):
-                video_id = video_data['media_id']
+            video_data = next(v for k,v in apollo_state.items() if v.get('slug', '') == episode_path)
+            video_id = video_data.get('mediaID')
+            if video_id:
+                requestor_info = self._downloader.cache.load('ap-mvpd', 'AdultSwim') or {}
+                authn_token = requestor_info.get('authn_token')
+                def is_expired(token, date_ele):
+                    token_expires = unified_timestamp(re.sub(r'[_ ]GMT', '', self._search_regex(
+                        '<%s>(.+?)</%s>' % (date_ele, date_ele), token, date_ele)))
+                    return token_expires and token_expires <= int(time.time())
+                if authn_token and is_expired(authn_token, 'simpleTokenExpires'):
+                    authn_token = None
+                auth_required = video_data.get('auth')
                 try:
                     info = self._extract_ngtv_info(
-                        video_id, {'appId':'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcHBJZCI6InRicy13ZWItMTVldWRlIiwicHJvZHVjdCI6InR2ZSIsIm5ldHdvcmsiOiJ0YnMiLCJwbGF0Zm9ybSI6IndlYiIsImlhdCI6MTUyMTU3MDE0Mn0.MWPtrJsU2E0LvPPyAd_-8vxwtRz6Fgs7P4lZlRf7HY8'}, {
+                        video_id, {'appId': initial_data.get('runtimeConfig', {}).get('TOP_APP_ID')}, 
+                        {
                             'url': url,
                             'site_name': 'AdultSwim',
-                            'auth_required': video_data.get('auth'),
+                            'auth_required': auth_required or authn_token is not None,
                         })
-                except:
-                    pass
-            video_id = video_data['id']
-
+                except Exception as e:
+                    if not auth_required:
+                        print(''.join(e.args))
+                        pass
+                    else:
+                        raise e
+            video_id = video_data['_id']
+#["id", "collection_title", "media_id", "poster", "title", "type"]
         streams_data = self._download_json(
-            'http://www.adultswim.com/videos/api/v3/videos/' + video_id + \
-            '?fields=title,type,duration,collection_title,poster,stream,segments,title_id',
-            video_id, 'Downloading JSON with m3u8 links')['data']
+            'http://www.adultswim.com/api/shows/v1/videos/' + video_id + \
+            '?fields=id,media_id,title,type,duration,collection_title,poster,stream,segments,title_id',
+            video_id, 'Downloading JSON with m3u8 links')['data']['video']
         duration = None
         chapters = []
         formats = []
@@ -199,16 +227,26 @@ class AdultSwimIE(TurnerBaseIE):
             'formats': formats,
         })
         if not is_stream:
+            if video_data.get('type') == "CLIP":
+                season_number = int_or_none(video_data.get('seasonNumber'))
+                season = None
+            else:                
+                season_id = next(k for k,v in apollo_state.items() if k.startswith('$Season:') and v.get('node', {}).get('id') == 'Video:'+video_data.get('id',''))
+                season_id = season_id.split('.')[0][1:]
+                season_number = apollo_state.get(season_id, {}).get('number')
+                season = apollo_state.get(season_id, {}).get('name')
             info.update({
                 'duration': info.get('duration') or duration or int_or_none(video_data.get('duration')),
-                'timestamp': info.get('timestamp') or int_or_none(video_data.get('launch_date')),
-                'season_number': info.get('season_number') or int_or_none(video_data.get('season_number')),
-                'episode_number': info.get('episode_number') or int_or_none(video_data.get('episode_number')),
+                'timestamp': info.get('timestamp') or parse_iso8601(video_data.get('launch_date')),
+                'season_number': info.get('season_number') or int_or_none(season_number),
+                'season': season,
+                'episode_number': info.get('episode_number') or int_or_none(video_data.get('episodeNumber')),
                 'subtitles': subtitles,
                 'chapters': info.get('chapters') or chapters,
                 'thumbnail': video_data.get('poster')
             })
 
-            info['series'] = video_data.get('collection_title') or info.get('series')
+            series = apollo_state.get(video_data.get('collection', {}).get('id'), {}).get('title')
+            info['series'] = series or info.get('series')
 
         return info
